2d4a18f6e49749b566e62c7a47b7421aabb8a08a
Joshua <joshuapruitt6484@gmail.com>
2024-10-03 15:03:16 -0700

    added delete addReaction and removeReaction to thoughtContoller.ts

--- src/controllers/thoughtController.ts
+++ src/controllers/thoughtController.ts
@@ -50,3 +50,94 @@ export const createThought = async (req: Request, res: Response) => {
         return;
     }
 }
+
+//update thought
+export const updateThought = async (req: Request, res: Response) => {
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$set: req.body},
+            {runValidators: true, new: true},
+        );
+
+        if(!thought){
+            return res.status(404).json({message: 'No thought found with id!'})
+        }
+
+        res.json(thought);
+        return;
+    } catch(err: any){
+        console.log(err);
+        res.status(500).json(err);
+        return;
+    }
+};
+
+//delete thought
+export const deleteThought = async(req: Request, res: Response) => {
+    try{
+        const thought = await Thought.findOneAndDelete({_id: req.params.thoughtId});
+        
+        if(!thought){
+            return res.status(404).json({message: 'No thought with this id found!'});
+        }
+
+        //update user to not have thought linked
+        const user = await User.findOneAndUpdate(
+            {thoughts: req.params.thoughtId},
+            {$pull: {thoughts: req.params.thoughtId}},
+            {new: true},
+        );
+
+        if(!user){
+            return res.status(404).json({message: 'Thought deleted but no user with this id!'});
+        }
+
+        res.json({message: 'Thought successfully deleted!'});
+        return;
+    } catch(err: any){
+        res.status(500).json(err);
+        return;
+    };
+};
+
+export const addReaction = async (req: Request, res: Response) => {
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$addToSet: {reactions: req.body}},
+            {runValidators: true, new: true},
+        );
+
+        if(!thought){
+            return res.status(404).json({message: 'No thought with this id!'});
+        }
+
+        res.json(thought);
+        return;
+    } catch(err: any){
+        res.status(500).json(err);
+        return;
+    }
+};
+
+export const removeReaction = async (req: Request, res: Response) => {
+    try {
+        const thought = await Thought.findOneAndUpdate(
+            {_id: req.params.thoughtId},
+            {$pull: {reactions: {reactionId: req.params.reactionId}}},
+            {runValidators: true, new: true},
+        );
+
+        if(!thought){
+            return res.status(404).json({message: 'Unable to delete reaction, No thought with this id!'});
+        }
+
+        res.json(thought);
+        return;
+    } catch(err){
+        res.status(500).json(err);
+        return;
+    }
+};
+
